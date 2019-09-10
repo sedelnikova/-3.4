0652e47a6cc4ae0297ecad2d196f05168db2c2f2
sedelnikova <54905384+sedelnikova@users.noreply.github.com>
2019-09-10 15:08:22 +0300

    Добавьте файлы проекта.

---
+++ Иван 3.4/Иван 3.4.cpp
@@ -0,0 +1,18 @@
+﻿// Иван 3.4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x, y;
+
+	cout << "Введите икс ";
+	cin >> x;
+	y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+
+	cout << "y = " << y << endl;
+	return 0;
+}
